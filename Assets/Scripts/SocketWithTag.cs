3b4b7a94ea49b672854823cd37e7dad2aa5d53b2
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2023-01-11 13:55:09 +0100

    added sockets / with comparetag

---
+++ Assets/Scripts/SocketWithTag.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class SocketWithTag : XRSocketInteractor
+{
+    [SerializeField] string targetTag = string.Empty;
+    public override bool CanHover(IXRHoverInteractable interactable)
+    {
+        return base.CanHover(interactable) && MatchUsingTag(interactable);
+    }
+    public override bool CanSelect(IXRSelectInteractable interactable)
+    {
+        return base.CanSelect(interactable) && MatchUsingTag(interactable);
+    }
+    private bool MatchUsingTag(IXRInteractable interactable)
+    {
+        return interactable.transform.CompareTag("targetTag");
+    }
+}
